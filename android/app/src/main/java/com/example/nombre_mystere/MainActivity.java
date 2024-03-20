87afd803a1710ac0b72ec9a43d823e680e4c5694
Yannis Barache <112858061+Yannis-barache@users.noreply.github.com>
2024-03-20 09:50:54 +0100

    Initial commit

---
+++ android/app/src/main/java/com/example/nombre_mystere/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.nombre_mystere;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/genNombre.dart
@@ -0,0 +1,7 @@
+
+import 'dart:math';
+
+int generateNombre(int max) {
+  final Random random = Random();
+  return random.nextInt(max);
+}
