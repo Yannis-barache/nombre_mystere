98344c735598f2403096cef4dd3b2574984f46c6
Yannis Barache <112858061+Yannis-barache@users.noreply.github.com>
2024-03-26 18:15:20 +0100

    affiche nb essais restants + ajout de scaffold pour aider le user

--- lib/pages/PageGame.dart
+++ lib/pages/PageGame.dart
@@ -52,6 +52,8 @@ class _PageGameState extends State<PageGame> {
     });
   }
 
+  int get essaisRestants => essaisMax != null ? essaisMax! - essais : 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,20 +64,7 @@ class _PageGameState extends State<PageGame> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Nombre mystère : $nombreMystere',
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
@@ -108,7 +97,7 @@ class _PageGameState extends State<PageGame> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Essais : $essais',
+                'Essais restants : $essaisRestants',
                 style: const TextStyle(
                   fontSize: 16,
                 ),
@@ -138,37 +127,92 @@ class _PageGameState extends State<PageGame> {
   }
 
   void _onPressed() {
-    final String nombreStr = leController.text;
-    debugPrint('Nombre : $nombreStr');
-    if (nombreStr.isEmpty || int.tryParse(nombreStr) == null) {
-      debugPrint('Invalid number');
-      return;
-    }
-    final int nombre = int.parse(nombreStr);
-    log("Nombre : $nombre - min : $minNiveau  -  max : $maxNiveau");
-    if (nombre < min! || nombre > max!) {
-      setState(() {
-        message = 'Le nombre doit être compris entre $min et $max';
-        log('Le nombre doit être compris entre $min et $max');
-      });
-      return;
-    }
-    final int resultat = Jeu.compareNombre(nombreMystere, nombre);
-    if (resultat == 0) {
-      debugPrint('Gagné');
-    } else {
-      setState(() {
-        essais = essais + 1;
-        if (resultat == 1) {
-          max = nombre;
-        } else {
-          min = nombre;
-        }
-        message = null;
-      });
-      if (essais == essaisMax) {
-        context.go('/lose');
+  final String nombreStr = leController.text;
+  leController.clear();
+  debugPrint('Nombre : $nombreStr');
+  if (nombreStr.isEmpty || int.tryParse(nombreStr) == null) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Center(
+          child: Text('Veuillez entrer un nombre valide',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+    return;
+  }
+  final int nombre = int.parse(nombreStr);
+  log("Nombre : $nombre - min : $minNiveau  -  max : $maxNiveau");
+  if (nombre < min! || nombre > max!) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Center(
+          child: Text('Le nombre doit être compris entre $min et $max',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+    return;
+  }
+  final int resultat = Jeu.compareNombre(nombreMystere, nombre);
+  if (resultat == 0) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Center(
+            child: Text('C\'est gagné !',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+    );
+  } else {
+    setState(() {
+      essais = essais + 1;
+      if (resultat == 1) {
+        max = nombre;
+        ScaffoldMessenger.of(context).removeCurrentSnackBar();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Center(
+              child: Text('Le chiffre est plus petit',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        );
+      } else {
+        min = nombre;
+        ScaffoldMessenger.of(context).removeCurrentSnackBar();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Center(
+              child: Text('Le chiffre est plus grand',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        );
       }
+      message = null;
+    });
+    if (essais == essaisMax) {
+      context.go('/lose');
     }
   }
+}
 }
